6a502087f62ea4d427b14bacb6c8fb1dfe58e8aa
IvannYad <yad7devadress@gmail.com>
2024-03-22 22:22:39 +0200

    feat: create UI for creating serpinskiy carpet form

--- src/pages/lab3-page/lab3-control-panel/Lab3ControlPanel.tsx
+++ src/pages/lab3-page/lab3-control-panel/Lab3ControlPanel.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./Lab3ControlPanel.scss"
 import SerpinskiyTriangleCreateForm from "./serpinskiy-triangle-create-form/SerpinskiyTriangleCreateForm"
+import SerpinskiyCarpetCreateForm from "./serpinskiy-carpet-create-form/SerpinskiyCarpetCreateForm";
 
 // type Lab3ControlPanelProps = {
 //    hello: string 
@@ -8,20 +9,28 @@ import SerpinskiyTriangleCreateForm from "./serpinskiy-triangle-create-form/Serp
 
 export default function Lab3ControlPanel(){
     const [isSerpTriangleFormOpen, setIsSerpTriangleFormOpen] = useState(false);
+    const [isSerpCarpetFormOpen, setIsSerpCarpetFormOpen] = useState(false);
 
     return (
         <div>
             <h2 className="lab3-title">Lab3</h2>
             <div className="control-panel-element-holder">
                 <div className="control-panel-button-holder">
-                    <button className="control-panel-button" onClick={() => setIsSerpTriangleFormOpen(!isSerpTriangleFormOpen)}>Serpinskiy triangle</button>
+                    <button className="control-panel-button" onClick={() => {
+                        setIsSerpCarpetFormOpen(false);
+                        setIsSerpTriangleFormOpen(!isSerpTriangleFormOpen);
+                    }}>Serpinskiy triangle</button>
                 </div>
                 <SerpinskiyTriangleCreateForm isOpen={isSerpTriangleFormOpen}/>
             </div>
             <div className="control-panel-element-holder">
                 <div className="control-panel-button-holder">
-                    <button className="control-panel-button">Serpinskiy triangle</button>
+                    <button className="control-panel-button" onClick={() => {
+                        setIsSerpTriangleFormOpen(false);
+                        setIsSerpCarpetFormOpen(!isSerpCarpetFormOpen);
+                    }}>Serpinskiy triangle</button>
                 </div>
+                <SerpinskiyCarpetCreateForm isOpen={isSerpCarpetFormOpen}/>
             </div>
             <div className="control-panel-element-holder">
                 <div className="control-panel-button-holder">
